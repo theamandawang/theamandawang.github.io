947ef9afd9c1810cafaa5a1f1d26adc3ce566562
Amanda Wang <52474776+theamandawang@users.noreply.github.com>
2022-08-23 00:36:15 -0400

    Styling changes for safari + sidebar transparency

--- my-portfolio/src/src/Gallery/Gallery.js
+++ my-portfolio/src/src/Gallery/Gallery.js
@@ -1,6 +1,7 @@
 import {useEffect, useRef} from 'react';
 import Sidebar from '../Sidebar/Sidebar';
-
+// https://medium.com/@amie.n.foster/flipping-out-how-to-create-a-card-that-flips-with-react-and-css-131dba54fc96
+// will be using above link to create gallery full of flipping cards!
 const Gallery = () => {
     const sidebarRef = useRef();
     const topRef = useRef();
